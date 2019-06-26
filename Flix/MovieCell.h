2db01f88f9f7c5faf3fef7cb3a526c4f9d1b5c16
Emily Hsiao <emily13hsiao@fb.com>
2019-06-26 15:10:15 -0700

    Added up to partial refresh functionality.

---
+++ Flix/MovieCell.h
@@ -0,0 +1,21 @@
+//
+//  MovieCell.h
+//  Flix
+//
+//  Created by emily13hsiao on 6/26/19.
+//  Copyright © 2019 emily13hsiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+
+@end
+
+NS_ASSUME_NONNULL_END

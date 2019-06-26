5d6bc0d1ac39e8e7b696a1aa95c350d89077fbe6
Emily Hsiao <emily13hsiao@fb.com>
2019-06-26 11:04:34 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by emily13hsiao on 6/26/19.
+//  Copyright © 2019 emily13hsiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by emily13hsiao on 6/26/19.
+//  Copyright © 2019 emily13hsiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

01be2b5ce9ea89e144d9f81d8d86770f0db4e494
Emily Hsiao <emily13hsiao@fb.com>
2019-06-26 17:45:40 -0700

    Created MoviesGridViewController class.

---
+++ Flix/Movies GridViewController.h
@@ -0,0 +1,17 @@
+//
+//  Movies GridViewController.h
+//  Flix
+//
+//  Created by emily13hsiao on 6/26/19.
+//  Copyright © 2019 emily13hsiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Movies_GridViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Flix/Movies GridViewController.m
@@ -0,0 +1,32 @@
+//
+//  Movies GridViewController.m
+//  Flix
+//
+//  Created by emily13hsiao on 6/26/19.
+//  Copyright © 2019 emily13hsiao. All rights reserved.
+//
+
+#import "Movies GridViewController.h"
+
+@interface Movies_GridViewController ()
+
+@end
+
+@implementation Movies_GridViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
